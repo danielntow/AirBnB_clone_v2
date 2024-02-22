715e6f653e1a18e138e11235d760304f23450348
danielntow <danielntow@gmail.com>
2024-02-22 13:24:05 +0000

    script that generates a .tgz archive from the contents of the web_static folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,33 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents
-of the web_static folder of your AirBnB Clone repo
-"""
+Script to setup Fabric and generate .tgz file
 
+"""
 from fabric.api import local
 from datetime import datetime
-import os
+
+from fabric.decorators import runs_once
 
 
+@runs_once
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder
-    Returns the archive path if successful, otherwise, returns None
+    Generates .tgz archive from the content
     """
-    try:
-        # Create the 'versions' folder if it doesn't exist
-        local("mkdir -p versions")
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} ./web_static".format(path))
 
-        # Generate the archive path
-        archive_path = "versions/web_static_{}.tgz".format(
-            datetime.now().strftime("%Y%m%d%H%M%S")
-        )
-
-        # Create the .tgz archive
-        local("tar -cvzf {} web_static".format(archive_path))
-
-        # Return the archive path if successful
-        return archive_path
-    except Exception:
-        # Return None if an error occurs
+    if result.failed:
         return None
+    return path
