494dda53a2a351a15342169b864abe4e1d3b7aa6
danielntow <danielntow@gmail.com>
2024-02-22 14:03:35 +0000

    compress file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-Script based on `1-pack_web_static.py` that distributes
-to web servers
+Script based on `1-pack_web_static.py` that distributes to web servers
 """
 
 import os
 from datetime import datetime
-from fabric.api import env, local, put, sudo, run, runs_once
-import re
+from fabric.api import env, local, put, run, runs_once
 
 env.hosts = ['44.200.178.183', '34.207.190.157']
 
@@ -15,7 +13,7 @@ env.hosts = ['44.200.178.183', '34.207.190.157']
 @runs_once
 def do_pack():
     """Generates an archive of the static files"""
-    if not os.path.isdir("versions"):
+    if not os.path.exists("versions"):
         os.mkdir("versions")
     c_time = datetime.now()
     output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
@@ -28,10 +26,11 @@ def do_pack():
     )
     try:
         print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} ./web_static".format(output))
+        local("tar -czvf {} ./web_static".format(output))
         archive_size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, archive_size))
-    except Exeption:
+    except Exception as e:
+        print("Error during packing: {}".format(e))
         output = None
     return output
 
@@ -48,18 +47,17 @@ def do_deploy(archive_path):
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace(".tgz", "")
     folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
+        put(archive_path, "/tmp/")
         run("mkdir -p {}".format(folder_path))
         run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
+        run("rm /tmp/{}".format(file_name))
         run("mv {}web_static/* {}".format(folder_path, folder_path))
         run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_path))
         print("New version deployed!")
-        success = True
-    except Exception:
-        success = False
-    return success
+        return True
+    except Exception as e:
+        print("Error during deployment: {}".format(e))
+        return False
