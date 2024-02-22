adb2a85cb2abd56e20856f20f384e3e17b2d8153
danielntow <danielntow@gmail.com>
2024-02-22 14:08:23 +0000

    compress file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,63 +1,58 @@
 #!/usr/bin/python3
+""" Write a Fabric script (based on the file
+1-pack_web_static.py) that distributes an archive
+to your web servers, using the function do_deploy
 """
-Script based on `1-pack_web_static.py` that distributes to web servers
-"""
-
-import os
+from fabric.api import local, env, run, put
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+from os.path import exists
 
 env.hosts = ['100.26.226.113', '34.234.193.247']
 
 
-@runs_once
 def do_pack():
-    """Generates an archive of the static files"""
-    if not os.path.exists("versions"):
-        os.mkdir("versions")
-    c_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        c_time.year,
-        c_time.month,
-        c_time.day,
-        c_time.hour,
-        c_time.minute,
-        c_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -czvf {} ./web_static".format(output))
-        archive_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archive_size))
-    except Exception as e:
-        print("Error during packing: {}".format(e))
-        output = None
-    return output
+    """[summary]"""
+    local('mkdir -p versions')
+    tar_dir = local("tar -czvf versions/web_static_{}.tgz web_static/".format((
+        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))), capture=True)
+
+    if tar_dir.succeeded:
+        return tar_dir
+    return None
 
 
 def do_deploy(archive_path):
-    """
-    Deploys the static files to the host servers
-    Args:
-        archive_path (str): The path to the archived static files
-    """
-    if not os.path.exists(archive_path):
-        return False
-
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print("New version deployed!")
+    """[summary]"""
+    # Returns False if the file at the path archive_path doesn’t exist
+    if exists(archive_path):
+        # archive_path = versions/web_static_#####.tgz
+        # file_path = web_static_#####.tgz
+        file_path = archive_path.split("/")[1]
+        # serv_path = /data/web_static/releases/web_static_#####
+        serv_path = "/data/web_static/releases/{}".format(
+            file_path.replace(".tgz", ""))
+        # Upload the archive to the /tmp/ directory of the web server
+        put('{}'.format(archive_path), '/tmp/')
+        # ???
+        run('mkdir -p {}'.format(serv_path))
+        # Uncompress the archive to the folde <..> on the web server
+        run('tar -xzf /tmp/{} -C {}/'.format(
+            file_path,
+            serv_path))
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(file_path))
+        # ???
+        run('mv -f {}/web_static/* {}/'.format(serv_path, serv_path))
+        # Delete the symbolic link <..> from the web server
+        run('rm -rf {}/web_static'.format(
+            serv_path))
+        # ??
+        run('rm -rf /data/web_static/current')
+        # run('unlink /data/web_static/current')
+        # Create a new Symbolic link, linked to the new version of your code
+        run('ln -s {} /data/web_static/current'.format(
+            serv_path))
+        # Retur  True if all operations have been done correctly
         return True
-    except Exception as e:
-        print("Error during deployment: {}".format(e))
+    else:
         return False
