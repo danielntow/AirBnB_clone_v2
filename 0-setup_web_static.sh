390a18019aceba6342be10b2ed7a462ffe91a269
danielntow <danielntow@gmail.com>
2024-02-22 12:41:57 +0000

    cript that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,64 @@
 #!/usr/bin/env bash
+# Sets up web server for deployment of web_static
 
-# Install Nginx if not already installed
-if ! command -v nginx &>/dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
+echo -e "\e[1;32m START\e[0m"
+
+# Update packages
+sudo apt-get update -y
+sudo apt-get install -y nginx
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
+
+# Allow incoming NGINX HTTP connections
+sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;32m Allowed incoming NGINX HTTP connections\e[0m"
+echo
+
+# Create directories and add test string
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "\e[1;32m Directories created\e[0m"
+echo "<h1>Welcome to DS ENTERPRISE</h1>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+echo -e "\e[1;32m Test string added\e[0m"
+
+# Prevent overwrite
+if [ -d "/data/web_static/current" ]; then
+    echo "Path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current
 fi
+echo -e "\e[1;32m Prevent overwrite\e[0m"
 
-# Create required folders if they don't exist
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-# Create a fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
-
-# Create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-# Give ownership to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
+# Create symbolic link and change ownership
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
 
 # Update Nginx configuration
-config_text="location /hbnb_static {
-    alias /data/web_static/current;
-    index index.html;
+nginx_config="server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By \$HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 }"
-sudo sed -i "/server {/a $config_text" /etc/nginx/sites-available/default
 
-# Restart Nginx
+echo "$nginx_config" | sudo tee /etc/nginx/sites-available/default >/dev/null
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+echo -e "\e[1;32m Symbolic link created\e[0m"
+
+# Restart NGINX
 sudo service nginx restart
-
-exit 0
+echo -e "\e[1;32m Restarted NGINX\e[0m"
