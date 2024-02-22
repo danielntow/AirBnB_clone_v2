e15d71858ba65adae13bb2b2b09909e15ad3c07c
danielntow <danielntow@gmail.com>
2024-02-22 14:41:35 +0000

    compress file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,51 +1,52 @@
 #!/usr/bin/python3
 """
-Script to distribute an archive to web servers
+Fabric script that distributes an archive to my two web servers
 """
 
-from fabric.api import put, run, env
-from os.path import exists
-from datetime import datetime
+from fabric.api import env, put, run
+import os
 
+# Servers' IP addresses
 env.hosts = ['100.26.226.113', '34.234.193.247']
+# The user to connect as
 env.user = 'ubuntu'
-env.key_filename = 'private_key.pem'
 
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to web servers
+    Distributes an archive to my two web servers
     """
-    if not exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
 
     try:
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
 
-        # Extract the archive to the folder /data/web_static/releases/<archive filename without extension>
-        filename = archive_path.split('/')[-1]
-        folder_path = "/data/web_static/releases/{}".format(
-            filename.split('.')[0])
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(filename, folder_path))
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
+
+        archive_filename = os.path.basename(archive_path)
+        archive_name_no_ext = archive_filename.split('.')[0]
+        releases_folder = "/data/web_static/releases/"
+        release_folder = releases_folder + archive_name_no_ext + "/"
+        run("mkdir -p {}".format(release_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
 
         # Delete the archive from the web server
-        run("rm /tmp/{}".format(filename))
+        run("rm /tmp/{}".format(archive_filename))
 
-        # Move contents to the parent folder and remove inner folder
-        run("mv {}/web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}/web_static".format(folder_path))
+        # Move contents of release_folder/web_static/ to release_folder/
+        run("mv {}web_static/* {}".format(release_folder, release_folder))
+        run("rm -rf {}web_static".format(release_folder))
 
         # Delete the symbolic link /data/web_static/current from the web server
         run("rm -rf /data/web_static/current")
 
-        # Create a new symbolic link linked to the new version of your code
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(release_folder))
 
         print("New version deployed!")
         return True
-
     except Exception as e:
-        print("Error: {}".format(e))
         return False
